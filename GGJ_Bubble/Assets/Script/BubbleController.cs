443a7fcd338e1f001be73d55f2cff62d18afe22c
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2025-01-21 15:36:48 -0300

    codigo

--- GGJ_Bubble/Assets/Script/BubbleController.cs
+++ GGJ_Bubble/Assets/Script/BubbleController.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class BubbleController : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    private bool frozen = false;
+    private float frezeEndTime;
+
+    public LayerMask wallLayer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+    private void Update()
+    {
+        if (frozen && Time.time > frezeEndTime)
+        {
+            DesactivateFreezePowerUp();
+        }
+    }
+    public void ActivateFreezePowerUp(float duracion)
+    {
+        frozen = true;
+        frezeEndTime = Time.time + duracion;
+        spriteRenderer.color = Color.cyan;
+        Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Paredes"), true);
+    }
+
+    public void DesactivateFreezePowerUp()
+    {
+        frozen = false;
+        spriteRenderer.color = originalColor;
+        Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Paredes"), false);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
