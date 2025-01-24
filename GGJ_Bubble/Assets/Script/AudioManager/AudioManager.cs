72358a5fa3788879fc4ed6549dd19e62f3516f5f
Camila Gonella <camilamgonella@gmail.com>
2025-01-24 00:22:31 -0300

    Menu done

--- GGJ_Bubble/Assets/Script/AudioManager/AudioManager.cs
+++ GGJ_Bubble/Assets/Script/AudioManager/AudioManager.cs
@@ -79,8 +79,8 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        if(_menuScene == true) PlayMusic(SoundType.MenuTheme, 1);
-        else PlayMusic(SoundType.LvlTheme, 0);
+        if(_menuScene == true) PlayMusic(SoundType.MenuTheme, 0.5f);
+        else PlayMusic(SoundType.LvlTheme, 0.5f);
     }
 
     public float GetMusicVolume()
--- GGJ_Bubble/Assets/Script/Menu.cs
+++ GGJ_Bubble/Assets/Script/Menu.cs
@@ -8,7 +8,7 @@ public class Menu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
-        AudioManager.instance.ChangeMusic(SoundType.LvlTheme, 0.2f);
+        AudioManager.instance.ChangeMusic(SoundType.LvlTheme, 0.7f);
     }
 
     public void ExitGame()
